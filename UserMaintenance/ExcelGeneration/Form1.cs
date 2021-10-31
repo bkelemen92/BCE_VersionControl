64aa73d5862501c6a3e16cb1e01151aea3f2f689
Kelemen Balázs <kelemen.balazs@andoc.net>
2021-10-31 20:55:04 +0100

    Load context from DB to local list

--- UserMaintenance/ExcelGeneration/Form1.cs
+++ UserMaintenance/ExcelGeneration/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExcelGeneration.Data;
 
 namespace ExcelGeneration
 {
     public partial class Form1 : Form
     {
+        List<Flat> flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+
+        private void LoadData()
+        {
+            flats = context.Flat.ToList();
         }
     }
 }
