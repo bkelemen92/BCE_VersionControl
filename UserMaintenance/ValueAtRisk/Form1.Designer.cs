dbaafd5457948b42d64cd3275ff02f32daaa8463
Kelemen Balázs <kelemen.balazs@andoc.net>
2021-11-01 17:13:54 +0100

    Var - FormDesign

--- UserMaintenance/ValueAtRisk/Form1.Designer.cs
+++ UserMaintenance/ValueAtRisk/Form1.Designer.cs
@@ -40,22 +40,22 @@ namespace ValueAtRisk
             this.dgv_Ticks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_Ticks.Location = new System.Drawing.Point(12, 12);
             this.dgv_Ticks.Name = "dgv_Ticks";
-            this.dgv_Ticks.Size = new System.Drawing.Size(350, 426);
+            this.dgv_Ticks.Size = new System.Drawing.Size(568, 426);
             this.dgv_Ticks.TabIndex = 0;
             // 
             // dgv_Portfolio
             // 
             this.dgv_Portfolio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_Portfolio.Location = new System.Drawing.Point(438, 12);
+            this.dgv_Portfolio.Location = new System.Drawing.Point(590, 12);
             this.dgv_Portfolio.Name = "dgv_Portfolio";
-            this.dgv_Portfolio.Size = new System.Drawing.Size(350, 426);
+            this.dgv_Portfolio.Size = new System.Drawing.Size(257, 426);
             this.dgv_Portfolio.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(859, 498);
             this.Controls.Add(this.dgv_Portfolio);
             this.Controls.Add(this.dgv_Ticks);
             this.Name = "Form1";
