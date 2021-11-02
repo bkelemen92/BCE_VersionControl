d21bdcfcefcd0a44ce4fb103920bbb24cababb80
Kelemen Balázs <kelemen.balazs@andoc.net>
2021-11-02 07:08:07 +0100

    Pdp - Toy abstract class created

---
+++ UserMaintenance/ProgramDesignPattern/Abstractions/Toy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ProgramDesignPattern.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = Height = 50;
+            Paint += Toy_Paint; ;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics graphics);
+
+        public virtual void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
